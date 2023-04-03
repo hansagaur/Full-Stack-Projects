7894bcde7d9d8df61ca88946a3b55b52de6f97d3
Hansa <63090127+hansagaur@users.noreply.github.com>
2023-04-03 12:21:44 +0530

    Add files via upload

---
+++ Hansa_Lab05_MVC/LMA-1/src/test/java/com/greatlearning/lma/Lma1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.lma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lma1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
