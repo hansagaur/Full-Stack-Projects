02949b611f3f541f1dd084887bdc8a9d50996c1d
Hansa <63090127+hansagaur@users.noreply.github.com>
2024-05-15 16:42:35 +0530

    ...

--- Hansa_Lab05_MVC/LMA-1/src/main/java/com/greatlearning/lma/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.lma.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.lma.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer>{
-
-}
