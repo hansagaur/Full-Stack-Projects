93da235d0278cf7cfa26c5e12c1388025b6025f4
Hansa <63090127+hansagaur@users.noreply.github.com>
2024-05-15 16:46:54 +0530

    ..

--- Hansa_Lab06_Spring_RestAPI/Hansa_Lab06_Spring_RestAPI-main/src/main/java/com/greatLearning/studentManagement/Exception/StudentException.java
+++
@@ -1,7 +0,0 @@
-package com.greatLearning.studentManagement.Exception;
-
-public class StudentException extends Exception{
-    public StudentException(String message) {
-        super(message);
-    }
-}
--- Hansa_Lab06_Spring_RestAPI/Hansa_Lab06_Spring_RestAPI-main/src/main/java/com/greatLearning/studentManagement/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatLearning.studentManagement.repository;
-
-import com.greatLearning.studentManagement.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
